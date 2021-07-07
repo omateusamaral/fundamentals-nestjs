5e862955ff9dc01d9a35b98ae03710ef1a425d26
mateusamarall <mateus.amaral018@gmail.com>
2021-07-07 09:29:28 -0300

    trabalhando com status code

--- src/controller/courses/courses.controller.ts
+++ src/controller/courses/courses.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses') //aqui a rota
 export class CoursesController {
   @Get('list') //passa aqui o endpoint necessário
-  findAll(): string {
-    return 'Listagem de cursos';
+  findAll(@Res() response): string {
+    return response.status(200).send('listagem do curso');
   }
 
   //parametros de rota
@@ -18,6 +27,14 @@ export class CoursesController {
   // pegar uma informação especifica
   // create(@Body('name') body) {
   create(@Body() body) {
+    return body;
+  }
+
+  //metodo http status code
+  @Post('create/status')
+  //httpsStatus tem todos status code
+  @HttpCode(HttpStatus.NO_CONTENT)
+  createStatus(@Body() body) {
     return body;
   }
 }
