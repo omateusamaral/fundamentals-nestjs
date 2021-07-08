57cc84808af0c2789961f0e8f08c216db0073859
mateusamarall <mateus.amaral018@gmail.com>
2021-07-08 10:22:35 -0300

    feito repositório de course e agora é possível fazer consultas no bd

--- src/service/courses/courses.service.ts
+++ src/service/courses/courses.service.ts
@@ -1,75 +1,69 @@
 import {
-  HttpException,
-  HttpStatus,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCourseDto } from 'src/controller/courses/dto/create-course.dto';
+import { UpdateCourseDto } from 'src/controller/courses/dto/update-course.dto';
 import { Course } from 'src/Model/course.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoursesService {
-  //por enquanto usaremos dados em memoria dps usaremos o bd
-  private courses: Course[] = [
-    {
-      id: 1,
-      name: 'Fundamentals NestJS',
-      description: 'Fundamentals NestJS',
-      tags: ['nodejs', 'nestjs', 'javascript'],
-    },
-  ];
-
-  //simulando buscas/criacao/edicao/deletar no bd
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {}
 
   findAll() {
-    return this.courses;
+    return this.courseRepository.find();
   }
 
   findOne(courseId: string) {
-    const course = this.courses.find(
-      (course) => course.id === Number(courseId),
-    );
+    const course = this.courseRepository.findOne({
+      where: {
+        id: courseId,
+      },
+    });
 
     if (!course) {
-      throw new HttpException(
-        `Course #${courseId} not founded`,
-        HttpStatus.NOT_FOUND,
-      );
+      throw new NotFoundException(`Course #${courseId} not founded`);
     }
     return course;
   }
-  create(createCourseDto: any) {
+  create(createCourseDto: CreateCourseDto) {
     try {
-      this.courses.push(createCourseDto);
-      return createCourseDto;
+      const createdCourse = this.courseRepository.create(createCourseDto);
+      return this.courseRepository.save(createdCourse);
     } catch (error) {
       throw new InternalServerErrorException(error);
     }
   }
-  update(courseId, updateCourseDto: any) {
-    const indexCourse = this.courses.findIndex(
-      (course) => course.id === Number(courseId),
-    );
-
-    if (indexCourse === -1) {
-      throw new HttpException(
-        `Couse ${courseId} not founded in our database`,
-        HttpStatus.NOT_FOUND,
-      );
-    }
-    this.courses[indexCourse] = updateCourseDto;
+  async update(courseId: string, updateCourseDto: UpdateCourseDto) {
+    await this.courseRepository
+      .createQueryBuilder()
+      .update()
+      .set({
+        ...updateCourseDto,
+      })
+      .where('id=:courseId', {
+        courseId,
+      })
+      .execute();
   }
 
-  delete(courseId: string) {
-    const indexCourse = this.courses.findIndex(
-      (course) => course.id === Number(courseId),
-    );
-    //indexCourse pode retornar -1 caso não encontrar nada
-    if (indexCourse === -1) {
-      throw new HttpException(
-        `Couse ${courseId} not founded in our database`,
-        HttpStatus.NOT_FOUND,
-      );
+  async delete(courseId: string) {
+    const course = await this.courseRepository.findOne({
+      where: {
+        id: courseId,
+      },
+    });
+
+    if (!course) {
+      throw new NotFoundException('Course not founded in our database');
     }
-    this.courses.splice(indexCourse, 1);
+
+    return this.courseRepository.remove(course);
   }
 }
